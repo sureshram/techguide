8ef370ba1edcd9e82010bbec67b858d2859d70dc
Suresh Ramakrishnaiah <suresh_sr@yahoo.com>
2014-12-11 19:58:04 -0800

    movehome.sh fixed permission issue #1

--- scripts/movehome.sh
+++ scripts/movehome.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-# Read here https://help.ubuntu.com/community/Partitioning/Home/Moving
+# Suresh Ramakrishnaiah - Tested with Ubuntu 14.04
+# Purpose: The purpose of the scrip is to move a directory to an existing partition
+# CAUTION: Please review the script carefully before executing it to understand the process
+# Read here for more details - https://help.ubuntu.com/community/Partitioning/Home/Moving
 
 DEST=$1
 mkdir ${DEST}
 sudo rsync -aXS --exclude='/*/.gvfs' /home/. ${DEST}
 sudo diff -r /home ${DEST}
-cd / && sudo mv /home /old_home && ln -s ${DEST} /home
+cd / && sudo mv /home /old_home && sudo ln -s ${DEST} /home
